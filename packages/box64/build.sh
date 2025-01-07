0d63572deadf4224e60a7792acba4834c1eae4ee
Oscar Simba <simbake2009@gmail.com>
2025-01-07 19:46:23 +0300

    Update build.sh

--- packages/box64/build.sh
+++ packages/box64/build.sh
@@ -3,7 +3,8 @@ TERMUX_PKG_DESCRIPTION="A dynamic translator for running x86_64 Linux programs o
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Test"
 TERMUX_PKG_VERSION=0.3.2
-TERMUX_PKG_SRCURL=https://github.com/ptitSeb/box64/archive/refs/tags/v0.3.2.tar.gz
-TERMUX_PKG_SHA256="8658b2c3840ae830ebb2b2673047d30a748139ec3afe178ca74a71adeddba63e"
-TERMUX_PKG_DEPENDS="libandroid-sysv-semaphore, libandroid-spawn, libandroid-spawn-static, libandroid-glob, clang"
+TERMUX_PKG_SRCURL=git+https://github.com/ptitSeb/box64/archive/refs/tags/v0.3.2.tar.gz
+TERMUX_PKG_GIT_BRANCH="main"
+#TERMUX_PKG_SHA256="8658b2c3840ae830ebb2b2673047d30a748139ec3afe178ca74a71adeddba63e"
+TERMUX_PKG_DEPENDS="libandroid-sysv-semaphore, libandroid-spawn, libandroid-spawn-static, libandroid-glob, clang, libandroid-posix-semaphore"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" -DCMAKE_INSTALL_PREFIX=$'{TERMUX_PREFIX}', -G Ninja -D TERMUX=1 -D CMAKE_C_COMPILER=clang -D CMAKE_BUILD_TYPE=RelWithDebInfo"
