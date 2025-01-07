e708f80f6435bc2ae4f78a17127f8f25d9ec41bd
Oscar Simba <simbake2009@gmail.com>
2025-01-07 16:41:45 +0300

    Update build.sh

--- packages/box64/build.sh
+++ packages/box64/build.sh
@@ -6,4 +6,4 @@ TERMUX_PKG_VERSION=0.3.2
 TERMUX_PKG_SRCURL=https://github.com/ptitSeb/box64/archive/refs/tags/v0.3.2.tar.gz
 TERMUX_PKG_SHA256="8658b2c3840ae830ebb2b2673047d30a748139ec3afe178ca74a71adeddba63e"
 TERMUX_PKG_DEPENDS="libandroid-sysv-semaphore, libandroid-spawn, libandroid-glob, ndk-sysroot, build-essential, libc++"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" -G Ninja -D TERMUX=1 -D CMAKE_C_COMPILER=clang -D CMAKE_BUILD_TYPE=RelWithDebInfo"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" -G Ninja -D TERMUX=1 -D CMAKE_C_COMPILER=clang -D CMAKE_BUILD_TYPE=RelWithDebInfo, -DCMAKE_INSTALL_PREFIX=$TERMUX_PREFIX"
