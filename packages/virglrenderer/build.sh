c99d386c69d9b0926ceda4d57512dc3837023979
Oscar Simba <simbake2009@gmail.com>
2025-01-10 02:52:33 +0300

    Update build.sh

--- packages/virglrenderer/build.sh
+++ packages/virglrenderer/build.sh
@@ -4,12 +4,12 @@ TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.1.0"
 TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://gitlab.freedesktop.org/virgl/virglrenderer/-/archive/virglrenderer-${TERMUX_PKG_VERSION}/virglrenderer-virglrenderer-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=8ed35fdfdffd75f8f06680b2599823b9d905df2f5be835c83e723a574c6a755d
+TERMUX_PKG_SRCURL=https://github.com/TrippleXC/VenusPatches/releases/download/Release/virglrenderer.tar.gz
+TERMUX_PKG_SHA256=27c49d6c090de3f08fec081de38727d1713a4981d4a2812b2b3e170b36da2cf0
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libdrm, libepoxy, libglvnd, libx11, mesa"
 TERMUX_PKG_BUILD_DEPENDS="xorgproto"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Dplatforms=egl,glx"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Dplatforms=egl,glx -Dvenus=true"
 
 termux_step_pre_configure() {
 	# error: using an array subscript expression within 'offsetof' is a Clang extension [-Werror,-Wgnu-offsetof-extensions]
