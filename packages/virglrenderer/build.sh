c000112f1eabaedb24d6a98fc86ee12dbd40034b
Oscar Simba <simbake2009@gmail.com>
2025-01-10 03:30:26 +0300

    Update build.sh

--- packages/virglrenderer/build.sh
+++ packages/virglrenderer/build.sh
@@ -7,7 +7,7 @@ TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/TrippleXC/VenusPatches/releases/download/Release/virglrenderer.tar.gz
 TERMUX_PKG_SHA256=27c49d6c090de3f08fec081de38727d1713a4981d4a2812b2b3e170b36da2cf0
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="libdrm, libepoxy, libglvnd, libx11, mesa"
+TERMUX_PKG_DEPENDS="libandroid-shmem, libxshmfence, vulkan-loader, vulkan-headers, libdrm, libepoxy, libglvnd, libx11, mesa"
 TERMUX_PKG_BUILD_DEPENDS="xorgproto"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Dplatforms=egl,glx -Dvenus=true"
 
@@ -16,3 +16,9 @@ termux_step_pre_configure() {
 	# list_for_each_entry_safe(struct vrend_linked_shader_program, ent, &shader->programs, sl[shader->sel->type])
 	CPPFLAGS+=" -Wno-error=gnu-offsetof-extensions"
 }
+termux_step_pre_configure() {
+termux_setup_cmake
+
+CPPFLAGS+=" -D__USE_GNU"
+LDFLAGS+=" -landroid-shmem"
+ }
